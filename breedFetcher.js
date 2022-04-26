d4d2e814f52df85c3453a60c928193e1a7b765d2
Konstantin <kostakv@outlook.com>
2022-04-26 19:19:02 +0000

    [feat] Added Mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,15 @@ var request = require("request");
 var BREED = process.argv.slice(2);
 
 var options = { method: 'GET',
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${BREED}` };
+  url: `https://api.thecatapi.com/v1/breeds/search?q=Siberian` };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+    
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+      console.log(typeof data);
+      
+    
+    });
 
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-  console.log(typeof data);
-  
-
-});
