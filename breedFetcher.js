c3e84b90311bef0ff465ce49a6304150069453f2
Konstantin <kostakv@outlook.com>
2022-04-26 19:00:50 +0000

    Added ignore file, updated breedFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,15 @@
+var request = require("request");
+var BREED = process.argv.slice(2);
+
+var options = { method: 'GET',
+  url: `https://api.thecatapi.com/v1/breeds/search?q=${BREED}` };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+  console.log(typeof data);
+  
+
+});
